178e585a6a2ba107a4793247e2b2b97bbd6682bd
Olena Androsovych <olenaandrosovych@macbook-air-olena.home>
2019-11-26 20:44:49 +0100

    Added models.py

--- pb_project/sub/models.py
+++ pb_project/sub/models.py
@@ -1,15 +1,28 @@
-# from django.db import models
+from django.db import models
 
-ARTISTS = {
-  'francis-cabrel': {'name': 'Francis Cabrel'},
-  'lej': {'name': 'Elijay'},
-  'rosana': {'name': 'Rosana'},
-  'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
-}
+class Contact(models.Model):
+  email = models.EmailField(max_length=100)
+  name = models.CharField(max_length=100)
+  
+class Category(models.Model):
+  name = models.CharField(max_length=100, unique=True)
+
+class Product(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.CharField(max_length=200)
+  score = models.CharField(max_length=1)
+  store = models.CharField(max_length=200)
+  url = models.URLField()
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Substitute(models.Model):
+  data = models.DataTimeField(auto_now_add = True)
+  favorite_list = models.BooleanField(default = False)
+  usual_product = models.ForeignKey(Product)
+  substitute_product = models.ForeignKey(Product)
+  contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
 
 
-ALBUMS = [
-  {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
-  {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
-  {'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
-]
+
+
+
